abf6ce4241049cd4ed8f9bcec7d6663f2a411a23
Anael Orlinski <ao@m9x.fr>
2024-09-12 02:45:52 +0200

    fix ucrt

--- libplatform/platform_win32.h
+++ libplatform/platform_win32.h
@@ -10,10 +10,11 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 // mingw needs this to enable some newer 64-bit functions
-#ifdef __MINGW32__
-#   undef  __MSVCRT_VERSION__
-#   define __MSVCRT_VERSION__ 0x800
-#endif
+// AO : this is not good with UCRT
+//#ifdef __MINGW32__
+//#   undef  __MSVCRT_VERSION__
+//#   define __MSVCRT_VERSION__ 0x800
+//#endif
 
 // set minimum win32 API requirement to Windows 2000 or higher
 #ifndef WINVER
